8eca857666acfeb039ff728e578f7f810fd7c321
Sieun Ju <33802191+sieunju@users.noreply.github.com>
2024-01-02 20:31:26 +0900

    fix: Local 상태에서 Swagger Test 안되는 이슈 처리

--- src/main/kotlin/com/hmju/til/core/config/SecurityConfig.kt
+++ src/main/kotlin/com/hmju/til/core/config/SecurityConfig.kt
@@ -2,17 +2,13 @@ package com.hmju.til.core.config
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import java.util.*
 
 
 /**
@@ -27,42 +23,36 @@ class SecurityConfig {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(this.javaClass) }
 
-    @Primary
-    @Bean("corsConfigurationSource")
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        return CorsConfigurationSource { req ->
-            logger.info("Security ${req.method} ${req.requestURI}")
-            logger.info("Cors Headers ${req.headerNames}")
-            val config = CorsConfiguration()
-            config.allowedHeaders = Collections.singletonList("*")
-            config.allowedMethods = Collections.singletonList("*")
-            config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:10004"))
-            config.allowCredentials = true
-            return@CorsConfigurationSource config
-        }
-    }
-
     @Bean
     fun securityFilterChain(
-        http: HttpSecurity,
-        @Qualifier("corsConfigurationSource") corsConfiguration: CorsConfigurationSource
+        http: HttpSecurity
     ): SecurityFilterChain {
-        http.httpBasic { it.disable() }
-        http.csrf { it.disable() }
-        http.cors {
-            it.configurationSource(corsConfiguration)
-        }
-        http.sessionManagement {
-            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        }
-        http.authorizeHttpRequests {
-            it.requestMatchers("/api/**").permitAll()
-            it.requestMatchers("/v3/api-docs/**").permitAll()
-            it.requestMatchers("/swagger-ui/**").permitAll()
-            // it.anyRequest().permitAll()
-            // it.requestMatchers("/swagger-ui/index.html").permitAll()
-            // it.anyRequest().permitAll()
-        }
-        return http.build()
+        return http
+            .cors {
+                it.configurationSource {
+                    val cors = CorsConfiguration()
+                    cors.allowedOrigins = listOf("*")
+                    cors.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    cors.allowedHeaders = listOf("*")
+                    cors
+                }
+            }
+            .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .headers { headers ->
+                headers.frameOptions { it.sameOrigin() }
+                headers.cacheControl { it.disable() }
+
+            }
+            .authorizeHttpRequests {
+                it.requestMatchers("/api/**").permitAll()
+                it.requestMatchers("/v3/api-docs/**").permitAll()
+                it.requestMatchers("/swagger-ui/**").permitAll()
+                // it.anyRequest().permitAll()
+                // it.requestMatchers("/swagger-ui/index.html").permitAll()
+                // it.anyRequest().permitAll()
+            }
+            .build()
     }
 }
