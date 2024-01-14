80915a320a37f3c8c775bc702c36332e38aea555
Sieun Ju <33802191+sieunju@users.noreply.github.com>
2024-01-14 22:13:05 +0900

    fix: Upload API Fetch 가져올때 host 추가해서 가져오도록 처리

--- src/main/kotlin/com/hmju/til/file/FileController.kt
+++ src/main/kotlin/com/hmju/til/file/FileController.kt
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -19,11 +20,7 @@ import org.springframework.web.multipart.MultipartFile
  */
 @Tag(name = "Uploads", description = "파일 업로드 API")
 @RestController
-@RequestMapping(
-    path = ["/api/v1/uploads"],
-    produces = [MediaType.APPLICATION_JSON_VALUE],
-    consumes = [MediaType.MULTIPART_FORM_DATA_VALUE]
-)
+@RequestMapping("/api/v1/uploads", produces = [MediaType.APPLICATION_JSON_VALUE])
 @Suppress("unused")
 class FileController @Autowired constructor(
     private val service: FileService
@@ -31,18 +28,21 @@ class FileController @Autowired constructor(
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(this.javaClass) }
 
+    @Value("\${remote_host}")
+    private lateinit var remoteHost: String
+
     @GetMapping
     fun fetch(
         @RequestParam(name = "pageNo", defaultValue = "1") pageNo: Int,
         @RequestParam(name = "pageSize", defaultValue = "30") pageSize: Int
     ): JSendResponse<FileDTO, PaginationMeta> {
         return JSendResponse.Builder<FileDTO, PaginationMeta>()
-            .setPayload(service.fetch(pageNo, pageSize).map { FileDTO(it) })
+            .setPayload(service.fetch(pageNo, pageSize).map { FileDTO(remoteHost, it) })
             .setMeta(service.fetchMeta(pageNo, pageSize))
             .build()
     }
 
-    @PostMapping
+    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun post(
         @RequestPart("files", required = true) files: List<MultipartFile>
     ): JSendResponse<FileDTO, JSendMeta> {
