b53d6e2b4cebb39959468066128c4ea0b069c686
Sieun Ju <33802191+sieunju@users.noreply.github.com>
2024-02-20 07:46:54 +0900

    fix: ktlint style 적용

--- src/main/kotlin/com/hmju/til/features/android/model/entity/AosMemo.kt
+++ src/main/kotlin/com/hmju/til/features/android/model/entity/AosMemo.kt
@@ -3,10 +3,10 @@ package com.hmju.til.features.android.model.entity
 import com.hmju.til.features.android.model.dto.AosMemoDTO
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.Table
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.time.LocalDateTime
 
 /**
